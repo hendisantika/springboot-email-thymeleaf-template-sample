ecde9bdc44a30880229b574d65b95b104bc52bab
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-09 03:10:44 +0700

    #9 - Add sendEmail method in EmailSenderService

--- src/main/java/com/hendisantika/springbootemailthymeleaftemplatesample/service/EmailSenderService.java
+++ src/main/java/com/hendisantika/springbootemailthymeleaftemplatesample/service/EmailSenderService.java
@@ -1,9 +1,18 @@
 package com.hendisantika.springbootemailthymeleaftemplatesample.service;
 
+import com.hendisantika.springbootemailthymeleaftemplatesample.model.Mail;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
 import org.thymeleaf.spring5.SpringTemplateEngine;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,4 +31,25 @@ public class EmailSenderService {
 
     @Autowired
     private SpringTemplateEngine templateEngine;
+
+    public void sendEmail(Mail mail) throws MessagingException, IOException {
+        MimeMessage message = emailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message,
+                MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
+                StandardCharsets.UTF_8.name());
+
+        helper.addAttachment("template-cover.png", new ClassPathResource("javabydeveloper-email.PNG"));
+
+        Context context = new Context();
+        context.setVariables(mail.getProps());
+
+        String html = templateEngine.process("newsletter-template", context);
+
+        helper.setTo(mail.getMailTo());
+        helper.setText(html, true);
+        helper.setSubject(mail.getSubject());
+        helper.setFrom(mail.getFrom());
+
+        emailSender.send(message);
+    }
 }
