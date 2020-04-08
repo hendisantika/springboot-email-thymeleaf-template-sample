ebb50d5c9b4bbe7c2844497204c4816029f01151
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-09 03:11:02 +0700

    #10 - Add ThymeleafTemplateConfig

---
+++ src/main/java/com/hendisantika/springbootemailthymeleaftemplatesample/config/ThymeleafTemplateConfig.java
@@ -0,0 +1,39 @@
+package com.hendisantika.springbootemailthymeleaftemplatesample.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-email-thymeleaf-template-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 09/04/20
+ * Time: 03.10
+ */
+@Configuration
+public class ThymeleafTemplateConfig {
+
+    @Bean
+    public SpringTemplateEngine springTemplateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.addTemplateResolver(htmlTemplateResolver());
+        return templateEngine;
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver htmlTemplateResolver() {
+        SpringResourceTemplateResolver emailTemplateResolver = new SpringResourceTemplateResolver();
+        emailTemplateResolver.setPrefix("/templates/");
+        emailTemplateResolver.setSuffix(".html");
+        emailTemplateResolver.setTemplateMode(TemplateMode.HTML);
+        emailTemplateResolver.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        return emailTemplateResolver;
+    }
+}
